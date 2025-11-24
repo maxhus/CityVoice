2d93f4e5cef54bb191c27077c00a4daf52fa3410
Malxhus <eliel.bgoss@gmail.com>
2025-11-24 12:47:16 +0100

    feat: Refactor routing to use AppRoutes component and update navigation paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Connexion from './pages/Connexion';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './router/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/connexion" element={<Connexion />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   );
 }
 
--- frontend/src/router/AppRoutes.js
+++ frontend/src/router/AppRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
-// import Login from '../pages/Login';
+import Connexion from '../pages/Connexion';
 // import Dashboard from '../pages/Dashboard';
 // import ReportForm from '../pages/ReportForm';
 // import MapView from '../pages/MapView';
@@ -14,15 +14,15 @@ const ProtectedRoute = ({ children }) => {
     return <div>Chargement...</div>;
   }
 
-  return user ? children : <Navigate to="/login" />;
+  return user ? children : <Navigate to="/connexion" />;
 };
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} /> */}
-      {/* <Route path="/register" element={<Register />} /> */}
+      <Route path="/connexion" element={<Connexion />} />
+      {/* <Route path="/inscription" element={<Register />} /> */}
       {/* <Route 
         path="/dashboard" 
         element={
